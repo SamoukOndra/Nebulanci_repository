f793ea020bee9e22efcc97a3cba28a188cbfdce2
SamoukOndra <platno@seznam.cz>
2023-10-25 14:38:35 +0200

    8.1.1
    bug fix

--- Nebulanci/Assets/00_Scripts/aaNotInUse/EventManager_InGame.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EventManager_InGame : MonoBehaviour
-{
-    public delegate void Attack(GameObject player);
-    public static event Attack OnAttack;
-}
