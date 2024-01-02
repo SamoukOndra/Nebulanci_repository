687481622f6298344770cd4842af86209778b7d3
SamoukOndra <platno@seznam.cz>
2024-01-02 22:28:35 +0100

    13
    pause bug fixed, bullet push ball

---
+++ Nebulanci/Assets/00_Scripts/05_Collision Materials/BallCM.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCM : CollisionMaterials
+{
+    [SerializeField] float pushForce = 100f;
+
+    Rigidbody rb;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void Interact(Vector3 hitPoint, Quaternion rotation, GameObject shootingPlayer = null)
+    {
+        Vector3 direction = (rotation * Vector3.forward).normalized;
+        rb.AddForceAtPosition(direction * pushForce, hitPoint);
+    }
+}
