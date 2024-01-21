96996ad436c1a4bce27356b8b976cac20ab2774e
SamoukOndra <platno@seznam.cz>
2024-01-21 11:48:14 +0100

    13.4
    konecne. endgame: rank, orderd models.
    
    no UI, effects... not yet

--- Nebulanci/Assets/00_Scripts/13_Results/GameStatistics.cs
+++ Nebulanci/Assets/00_Scripts/13_Results/GameStatistics.cs
@@ -46,35 +46,26 @@ public class GameStatistics : MonoBehaviour//, IComparable
     private void InitializeResultCharacterPlaceholder(GameObject newPlayer, PlayerBlueprint playerBlueprint)
     {
         GameObject resCharPlaceholderGO = Instantiate(resultCharacterPlaceholder);
-        resultCharacterPlaceholders.Add(resCharPlaceholderGO);
 
         ResultCharacterPlaceholder resCharPlaceholderScript = resCharPlaceholderGO.GetComponent<ResultCharacterPlaceholder>();
 
         GameObject character = Instantiate(availableCharacters[playerBlueprint.characterIndex], resCharPlaceholderGO.transform, false);
         resCharPlaceholderScript.AddCharacter(character);
-        //character.layer = resultCamLayer;
         Util.SetLayerToAllChildren(resCharPlaceholderGO.transform, resultCamLayer);
 
         resCharPlaceholderScript.SetAnimatorControler();
-
-        //PlayerStatistics playerStatistics = new(newPlayer, playerBlueprint.name);
-        //resCharPlaceholderScript.AddPlayerStatistics(playerStatistics);
-        //playerStatisticsList.Add(playerStatistics);
 
         PlayerStatistics ps = resCharPlaceholderGO.AddComponent<PlayerStatistics>();
         ps.SetPlayerAndName(newPlayer, playerBlueprint.name);
         playerStatisticsList.Add(ps);
 
-        //resCharPlaceholderGO.SetActive(false);
         resCharPlaceholderScript.ActivateCharacter(false);
-
-        
     }
 
     public void EndGame()
     {
         DecidePlayerRank();
-        //SortResultPlaceholders();
+        SortResultPlaceholders();
         SpawnResultPlaceholders();
         resultCam.SetActive(true);
     }
@@ -84,19 +75,13 @@ public class GameStatistics : MonoBehaviour//, IComparable
     {
         
 
-        List<int> orderdScores = GetOrderedFinalScoresAndOrderPlaceholders();
-
-        Debug.Log("ordered scores: " + orderdScores);
+        List<int> orderdScores = GetOrderedFinalScores();
 
         SetRanks();
-
-        //methods//
         
-        List<int> GetOrderedFinalScoresAndOrderPlaceholders() //I am sorry for anti-SOLID :,(
+        List<int> GetOrderedFinalScores()
         {
             List<int> _finalScores = new();
-            Dictionary<int, int> _scoreIndexDict = new();
-            List<GameObject> _orderedPlaceholders = new();
 
             Debug.Log("pa: " + playersAmount);
             Debug.Log("psl.count: " + playerStatisticsList.Count);
@@ -106,23 +91,10 @@ public class GameStatistics : MonoBehaviour//, IComparable
                 int _score = playerStatisticsList[i].finalScore;
 
                 _finalScores.Add(_score);
-                if(!_scoreIndexDict.ContainsKey(_score))
-                    _scoreIndexDict.Add(_score, i);
-                Debug.Log("player " + (i+1) + "; final score: " + _score);
             }
 
             _finalScores.Sort();
             _finalScores.Reverse();
-
-            for (int j = 0; j < playersAmount; j++)
-            {
-                if(_scoreIndexDict.TryGetValue(_finalScores[j], out int index))
-                {
-                    _orderedPlaceholders.Add(resultCharacterPlaceholders[index]);
-                }
-            }
-
-            resultCharacterPlaceholders = _orderedPlaceholders;
 
             return _finalScores;
         }
@@ -130,21 +102,25 @@ public class GameStatistics : MonoBehaviour//, IComparable
         void SetRanks()
         {
             foreach(PlayerStatistics ps in playerStatisticsList)
-            {
-                
+            {     
                 int rank = (orderdScores.IndexOf(ps.finalScore))+1;
                 ps.rank = rank;
-
-                Debug.Log("name in playerStatistics: " + ps.playerName + "; rank: " + ps.rank); //ok
             }
         }
     }
 
-    //private void SortResultPlaceholders()
-    //{
-    //    //nevimjaksetodela/*resultCharacterPlaceholders =*/ resultCharacterPlaceholders.OrderBy(GameObject => GameObject.GetComponent<ResultCharacterPlaceholder>().GetRank()).ToList();
-    //
-    //}
+    private void SortResultPlaceholders()
+    {
+        IEnumerable<PlayerStatistics> orderedStatistics = playerStatisticsList.OrderBy(stat => stat.rank);
+        orderedStatistics.ToList();
+        
+        foreach(PlayerStatistics ps in orderedStatistics)
+        {
+            Debug.Log("sorted: name: " + ps.playerName + "; psrank: " + ps.rank);
+
+            resultCharacterPlaceholders.Add(ps.gameObject);
+        }
+    }
 
     private void SpawnResultPlaceholders()
     {
@@ -152,11 +128,8 @@ public class GameStatistics : MonoBehaviour//, IComparable
         for(int i = 0; i < playersAmount; i++)
         {
             resultCharacterPlaceholders[i].transform.position = placeholderOffset * i;
-            //resultCharacterPlaceholders[i].SetActive(true);
             resultCharacterPlaceholders[i].GetComponent<ResultCharacterPlaceholder>().ActivateCharacter(true);
-            HandleResultsTargetGroup.singleton.AddTarget(resultCharacterPlaceholders[i]);
-
-            //Debug.Log("rank: " + (resultCharacterPlaceholders[i].GetComponent<ResultCharacterPlaceholder>().GetRank())); hlasi error, ale je to picovina
+            HandleResultsTargetGroup.singleton.AddTarget(resultCharacterPlaceholders[i]);            
         }
     }
 
